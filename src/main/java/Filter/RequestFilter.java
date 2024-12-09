2924a04f64949822e7a82dc632f0d4828bed396d
andrey551 <76159085+andrey551@users.noreply.github.com>
2024-12-09 21:06:37 +0300

    Doc: Add report phase 1

--- src/main/java/Filter/RequestFilter.java
+++ src/main/java/Filter/RequestFilter.java
@@ -1,22 +1,17 @@
 package Filter;
 
 import Database.AccountTableRemote;
-import Utils.jwtHandler;
 import jakarta.annotation.Priority;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
-import java.net.http.HttpHeaders;
 
 @Provider
 @Priority(value=30)
 public class RequestFilter implements ContainerRequestFilter {
-    @EJB
-    private AccountTableRemote accountTableRemote;
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
